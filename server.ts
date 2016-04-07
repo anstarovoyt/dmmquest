f7f9b849904c952cf2ad84b56f99938db0fa00c5
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2016-04-08 02:57:33 +0300

    added port

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ var serveStatic = require('serve-static');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-var PORT = 8082;
+var PORT = process.env.PORT || 8080;
 var TARGET_PATH_MAPPING = {
     BUILD: './build',
     DIST: './dist'
