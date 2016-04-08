0b0c53c49faf5371476f589c47ab9fcdb760bd26
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2016-04-08 03:04:44 +0300

    improve

--- client/app/components/stage/StageLocked.tsx
+++ client/app/components/stage/StageLocked.tsx
@@ -1,11 +1,16 @@
 import * as React from "react"
+import {Link} from 'react-router'
+
 
 export class StageLocked extends React.Component<any, any> {
 
     render() {
 
-        return <div>
-            Этап недоступен
+        return  <div className="col-lg-12">
+            <h1><Link to="/">
+                <span className="glyphicon glyphicon-arrow-left"></span>
+            </Link>
+                <span>Этап недоступен</span></h1>
         </div>
     }
 }
