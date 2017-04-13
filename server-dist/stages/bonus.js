5a855ecc6d8098229631ef1ee03e05d5888e3598
anstarovoyt <andrey.starovoyt@jetbrains.com>
2017-04-14 02:16:22 +0500

    other files

---
+++ server-dist/stages/bonus.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.stage = {
+    status: 3 /* BONUS */,
+    name: "Для задротов",
+    internalName: "Бонус",
+    quests: []
+};
+//# sourceMappingURL=bonus.js.map
---
+++ server-dist/stages/killer.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.stage = {
+    status: 5 /* KILLER */,
+    name: "Убийца",
+    internalName: "Killer",
+    quests: [
+        "Кто",
+        "Где",
+        "Когда"
+    ]
+};
+//# sourceMappingURL=killer.js.map
