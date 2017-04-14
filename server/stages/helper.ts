2536c2e8713cf77578acec9348faa1d8824da011
anstarovoyt <andrey.starovoyt@jetbrains.com>
2017-04-14 19:06:27 +0500

    videos

--- server/stages/helper.ts
+++ server/stages/helper.ts
@@ -1,11 +1,11 @@
 export function getVideo(video: boolean, ...links: string[]) {
     let result = '';
 
-    let height = video ? "182" : "50";
+    let height = video ? "130" : "50";
 
     links.forEach(el => {
         result += "<br>";
-        result += `<iframe width=\"300\" height=\"${height}\" src=\"${el}\?modestbranding=1&autohide=0&showinfo=0" frameborder=\"0\" allowfullscreen></iframe>`;
+        result += `<iframe width=\"200\" height=\"${height}\" src=\"${el}\?modestbranding=1&autohide=0&showinfo=0" frameborder=\"0\" allowfullscreen></iframe>`;
     });
 
     result += "<br>";
