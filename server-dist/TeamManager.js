eb31a763c0e127763d2b0b20c17ed69a586a372b
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2017-04-22 21:21:39 +0300

    inc password

--- server-dist/TeamManager.js
+++ server-dist/TeamManager.js
@@ -117,7 +117,7 @@ var TeamManager = (function () {
     TeamManager.makeid = function () {
         var text = '';
         var possible = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        for (var i = 0; i < 8; i++)
+        for (var i = 0; i < 10; i++)
             text += possible.charAt(Math.floor(Math.random() * possible.length));
         return text;
     };
--- server/TeamManager.ts
+++ server/TeamManager.ts
@@ -145,7 +145,7 @@ export class TeamManager {
         let text = '';
         const possible = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-        for (let i = 0; i < 8; i++)
+        for (let i = 0; i < 10; i++)
             text += possible.charAt(Math.floor(Math.random() * possible.length));
 
         return text;
