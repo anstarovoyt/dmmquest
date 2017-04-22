e85b12c6ea749f53f0d096cde6862c8dc3776303
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2017-04-22 13:01:29 +0300

    show /hide block

---
+++ client/app/components/admin/AnswersComponent.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export class AnswersComponent extends React.Component<{ info: TeamInfo }, {}> {
+
+    render() {
+        return <div>
+
+        </div>;
+    }
+}
--- client/app/components/admin/TeamInfo.tsx
+++ client/app/components/admin/TeamInfo.tsx
@@ -1,9 +1,20 @@
-import * as React from "react";
-import {auth} from "../../authentication/AuthService";
-import {appStateService} from "../../state/AppStateService";
-import {removeTeam, unlockLastStage} from "../../communitation/Dispatcher";
+import * as React from 'react';
+import {auth} from '../../authentication/AuthService';
+import {appStateService} from '../../state/AppStateService';
+import {removeTeam, unlockLastStage} from '../../communitation/Dispatcher';
 
-export class TeamInfoComponent extends React.Component<{info:TeamInfo, reloadParent:() => void}, any> {
+const Link: ReactRouter.Link = require('react-router/lib/Link');
+
+export class TeamInfoComponent extends React.Component<{ info: TeamInfo, reloadParent: () => void }, { showInfo: boolean }> {
+
+
+    constructor(props: { info: TeamInfo; reloadParent: (() => void) }, context: any) {
+        super(props, context);
+
+        this.state = {
+            showInfo: false
+        };
+    }
 
     render() {
         var teamInfo = this.props.info;
@@ -15,38 +26,50 @@ export class TeamInfoComponent extends React.Component<{info:TeamInfo, reloadPar
         var unlockElement = this.getUnlockElement();
         return (
             <div className="row">
-                <div className="col-xs-12 col-md-8">
+                <div className="col-xs-18 col-md-12">
                     <h3>Команда {team.name}</h3>
                     <p><b>Секретный код</b>: {team.secretCode}</p>
-                    <p><b>Токен</b>: {team.tokenId}</p>
-                    <p><b>Начальный этап (отсчет от 1)</b>: {team.startFromStage + 1}</p>
-                    <p>
-                        Время первого логина {teamInfo.firstLoginDateEkbTimezone ? teamInfo.firstLoginDateEkbTimezone : " — не определено"}
-                        <br />
-                        Ожидаемое время окончания {teamInfo.endQuestEkbTimezone ? teamInfo.endQuestEkbTimezone : " — не определено"}
-                    </p>
-                    {answers}
+                    <h4><a href="#"
+                           className="hideShowLink"
+                           onClick={this.hideOrShowPanel.bind(this)}>
+                        {this.state.showInfo ? 'Hide info' : 'Show info'}
+                    </a></h4>
+                    <br/>
 
-                    <br />
-                    <br />
-                    <div>
-                        {unlockElement ? <h4>{unlockElement}</h4> : ""}
-                        {team.tokenId == auth.getToken() ? <h4>Это вы</h4> :
-                            <h4><a href="#"
-                                   onClick={this.removeTeam.bind(this)}>Удалить</a>
-                            </h4>}
+                    {this.state.showInfo ?
+                        <div>
+                            <p><b>Токен</b>: {team.tokenId}</p>
+                            <p><b>Начальный этап (отсчет от 1)</b>: {team.startFromStage + 1}</p>
+                            <p>
+                                Время первого
+                                логина {teamInfo.firstLoginDateEkbTimezone ? teamInfo.firstLoginDateEkbTimezone : ' — не определено'}
+                                <br />
+                                Ожидаемое время
+                                окончания {teamInfo.endQuestEkbTimezone ? teamInfo.endQuestEkbTimezone : ' — не определено'}
+                            </p>
+                            {answers}
 
-                    </div>
+                            <br />
+                            <br />
+                            <div>
+                                {unlockElement ? <h4>{unlockElement}</h4> : ''}
+                                {team.tokenId == auth.getToken() ? <h4>Это вы</h4> :
+                                    <h4><a href="#"
+                                           onClick={this.removeTeam.bind(this)}>Удалить</a>
+                                    </h4>}
+
+                            </div>
+                        </div> : ''}
+
                 </div>
-
             </div>
-        )
+        );
     }
 
     getUnlockElement() {
         var teamInfo = this.props.info;
 
-        var lastCompletedStage:Stage = null;
+        var lastCompletedStage: Stage = null;
         for (var stage of teamInfo.appState.stages) {
             if (stage.status == StageStatus.COMPLETED) {
                 lastCompletedStage = stage;
@@ -59,7 +82,7 @@ export class TeamInfoComponent extends React.Component<{info:TeamInfo, reloadPar
 
 
         return <a href="#"
-                  onClick={this.unlockLastStage.bind(this)}>Разблокировать последний этап</a>
+                  onClick={this.unlockLastStage.bind(this)}>Разблокировать последний этап</a>;
     }
 
     unlockLastStage(e) {
@@ -75,7 +98,14 @@ export class TeamInfoComponent extends React.Component<{info:TeamInfo, reloadPar
             if (res.success) {
                 this.props.reloadParent();
             }
-        })
+        });
+    }
+
+    hideOrShowPanel(e) {
+        e.preventDefault();
+        this.setState({
+            showInfo: !this.state.showInfo
+        });
     }
 
     removeTeam(e) {
@@ -92,7 +122,7 @@ export class TeamInfoComponent extends React.Component<{info:TeamInfo, reloadPar
             if (res.success) {
                 this.props.reloadParent();
             }
-        })
+        });
     }
 
     getStatus() {
@@ -100,7 +130,7 @@ export class TeamInfoComponent extends React.Component<{info:TeamInfo, reloadPar
         var teamInfo = this.props.info;
         var i = 0;
         for (var stage of teamInfo.appState.stages) {
-            result.push(this.getStageElement(stage))
+            result.push(this.getStageElement(stage));
 
         }
 
@@ -110,46 +140,46 @@ export class TeamInfoComponent extends React.Component<{info:TeamInfo, reloadPar
     }
 
 
-    getStageElement(stage:Stage) {
-        var stageNumber = stage.status == StageStatus.BONUS ? "Бонус" : Number(stage.id) + 1;
+    getStageElement(stage: Stage) {
+        var stageNumber = stage.status == StageStatus.BONUS ? 'Бонус' : Number(stage.id) + 1;
         return <span key={stage.id}>
                 <b>Этап {stageNumber}</b>: {appStateService.getStageNameById(stage.id)}
             — <b>{this.getStageStatusText(stage)}</b>
             <div>
                 {this.getAnswers(stage)}
             </div>
-                </span>
+                </span>;
     }
 
     getStageStatusText(stage) {
         var status = stage.status;
         if (status == StageStatus.OPEN) {
-            return "В процессе"
+            return 'В процессе';
         }
 
         if (status == StageStatus.COMPLETED) {
-            var res = "Сдан";
+            var res = 'Сдан';
             if (stage.closedTime) {
-                res += " (" + stage.closedTime + ")";
+                res += ' (' + stage.closedTime + ')';
             }
             return res;
         }
 
         if (status == StageStatus.BONUS) {
-            return "Бонус"
+            return 'Бонус';
         }
 
-        return "Не открыт"
+        return 'Не открыт';
     }
 
-    getAnswers(stage:Stage) {
+    getAnswers(stage: Stage) {
         var result = [];
 
         var questAnswers = stage.questAnswers;
         for (var answerId in questAnswers) {
             if (questAnswers.hasOwnProperty(answerId)) {
                 result.push(<p key={answerId}>&nbsp;&nbsp;&nbsp;Вопрос {Number(answerId) + 1}
-                    — {questAnswers[answerId].answer} </p>)
+                    — {questAnswers[answerId].answer} </p>);
             }
         }
 
